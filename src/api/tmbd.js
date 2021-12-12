97e6ffd176baf570f031111d5793e2b72aa3c91a
SskyWalkerR <sammanualvarghese@gmail.com>
2021-12-12 08:38:21 +0530

    .gitignore is now working

--- src/api/tmbd.js
+++ src/api/tmbd.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3/";
 // const IMG_URL = "https://image.tmdb.org/t/p/original/";
-const KEY = process.env.REACT_APP_TMDB_API_KEY;
+const KEY = process.env.REACT_APP_TMDB_KEY;
 
 export const request = axios.create({
    baseURL: BASE_URL,
